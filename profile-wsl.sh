14f15f2718f1237835c25cb8b69157ad4959c521
Benoit Joly <benoit@benoitj.ca>
2021-06-11 15:05:04 -0400

    more changes to wsl profile

--- profile-wsl.sh
+++ profile-wsl.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-set -euo
-
 # detect IP address. may need tweaking
 ip="$(powershell.exe -Command 'Get-NetIPAddress | Where-Object -FilterScript { $_.ValidLifetime -Lt ([TimeSpan]::FromDays(1)) }' | grep IPAddress | tr -d '\r\n')"
 ip="${ip##*: }"
@@ -8,7 +5,7 @@ echo "Detected IP: $ip"
 
 export DISPLAY="${ip}:0"
 
-type xauth || sudo apt install -y xauth pwgen coreutils
+type xauth >/dev/null || sudo apt install -y xauth pwgen coreutils
 
 # setup the xauth magic cookie
 magicCookie="$(pwgen 30 1 | md5sum)"
@@ -16,7 +13,7 @@ magicCookie="${magicCookie% *}"
 xauth add "$DISPLAY" . "$magicCookie"
 
 # finds the windows user path
-userProfile="$(wslpath $(cmd.exe /C \"echo %USERPROFILE%\" 2>/dev/null | tr -d '\r\n'))"
+userProfile="$(wslpath $(cmd.exe /C ""echo %USERPROFILE%"" 2>/dev/null | tr -d '\r\n'))"
 
 echo "copying .Xauthority to $userProfile"
 cp ~/.Xauthority "$userProfile"
