78dbe8e4c4e20cdd9969e0b011212d178ce44055
Benoit Joly <benoit@benoitj.ca>
2021-06-12 18:56:04 -0400

    updated run script to start or run

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-set -e
 
-${DOCKER:-docker} run -it \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v "$HOME/.Xauthority:/home/$USER/.Xauthority" \
-    -v "$(pwd)/init.d:/init.d" \
-    -e DISPLAY="$DISPLAY" \
-    -h "$HOSTNAME" \
-    benoitj/docker-emacs-dotfiles:latest "$@"
+if test "$(docker ps -qa -f name=docker-emacs)" != ""; then
+    docker start docker-emacs
+else
+    docker run -it \
+        --name "docker-emacs" \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v "$HOME/.Xauthority:/home/$USER/.Xauthority" \
+        -v "$(pwd)/init.d:/init.d" \
+        -e DISPLAY="$DISPLAY" \
+        -h "$HOSTNAME" \
+        benoitj/docker-emacs-dotfiles:latest "$@"
+fi
